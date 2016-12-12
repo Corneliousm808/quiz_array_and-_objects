16c3ab81898f62092bd50ec29351cb36c76646cc
Corneliousm808 <corneliousmooring@gmail.com>
2016-12-12 08:26:18 -1000

    i need help

---
+++ quiz_array_and_objects.js
@@ -0,0 +1,20 @@
+/*
+Cornelious Mooring
+ Dec. , 7, 2016
+*/
+
+var myArray = [10, "true"]
+
+console.log(10)
+
+function passing(myArray) {
+  if (myArray == true){
+    console.log()
+  }
+  else
+    console.log("you shall not pass!")
+}
+
+myArray(10, true)
+
+var batman = ["man", "utilityBelt", "batmobile", "robin"]
